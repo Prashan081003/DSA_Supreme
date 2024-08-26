a9f388e50b3dddde6ab0017f8859987b8bb78411
Prashan081003 <121500232+Prashan081003@users.noreply.github.com>
2024-08-26 13:24:41 +0530

    Create Minimum Size Subarray Sum.cpp

---
+++ Sliding Window Technique/Minimum Size Subarray Sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+            int len = INT_MAX ; 
+                int currSum =0 ; 
+                  int s=0 ,e =0 ; 
+                     int sum = 0 ; 
+
+             while(e<nums.size()){
+                 //sum me include karo value
+                 sum+=nums[e];
+                 //ho skta hai ,sum==target 
+                  //ho skta hai ,sum>target  
+                  //ho skta hai ,sum<target 
+
+                  //minimise->equal or greater
+                  //explore ->lesser
+
+                  while(sum>=target){
+                     //minimise
+                     len = min(len,e-s+1);
+                     sum-=nums[s]; 
+                     s++ ; 
+                  }
+                e++; 
+             }
+
+            return len==INT_MAX?0:len; 
+    }
+};
