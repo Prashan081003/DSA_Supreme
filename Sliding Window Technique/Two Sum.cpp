090556538db64435cf26c69ba2cf4754a227ec72
Prashan081003 <121500232+Prashan081003@users.noreply.github.com>
2024-09-02 05:43:51 +0530

    Update Two Sum.cpp

--- Sliding Window Technique/Two Sum.cpp
+++ Sliding Window Technique/Two Sum.cpp
@@ -1,5 +1,48 @@
 //    Leetcode-- 1 ====> Easy ; 
 
+//<<<<===========  Method 1 ================ using custom sortin =========>>>>>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int, int>> vec; // Pair to store (index, value)
+        vector<int> ans;
+        
+        // Fill the vector with pairs of (index, value)
+        for (int i = 0; i < nums.size(); i++) {
+            vec.push_back({i, nums[i]});
+        }
+        
+        // Custom sort: sort by value in ascending order
+        sort(vec.begin(), vec.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
+            return a.second < b.second; // Compare based on the value
+        });
+        
+        int s = 0, e = nums.size() - 1;
+        
+        // Use two-pointer technique to find the two values that add up to the target
+        while (s < e) {
+            int sum = vec[s].second + vec[e].second; // Sum the values
+            if (sum == target) {
+                ans.push_back(vec[s].first); // Store the original indices
+                ans.push_back(vec[e].first);
+                return ans;
+            }
+            else if (sum > target) {
+                e--;
+            }
+            else {
+                s++;
+            }
+        }
+        
+        return ans; // Return an empty vector if no solution is found
+    }
+};
+//Method 2 ===========  without custom sorting ===========>>>>>>>>>>>>
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
