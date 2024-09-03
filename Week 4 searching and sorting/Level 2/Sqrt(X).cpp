4bbc966db5684e3af4aa976e19fd2e641ca2f057
Prashan081003 <121500232+Prashan081003@users.noreply.github.com>
2024-09-03 16:21:13 +0530

    Update Sqrt(X).cpp

--- Week 4 searching and sorting/Level 2/Sqrt(X).cpp
+++ Week 4 searching and sorting/Level 2/Sqrt(X).cpp
@@ -31,3 +31,47 @@ public:
         return ans;
     }
 };
+
+
+#include <cmath>
+
+class Solution {
+public:
+    // Function to calculate the integer part of the square root
+    int integerSqrt(int x) {
+        int s = 0;
+        int e = x;
+        long long int mid = s + (e - s) / 2;
+        int ans = -1;
+
+        while (s <= e) {
+            if (mid * mid == x) {
+                return mid;
+            } else if (mid * mid < x) {
+                ans = mid;
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
+            mid = s + (e - s) / 2;
+        }
+        return ans;
+    }
+
+    // Function to calculate the square root up to three decimal places
+    double mySqrt(int x) {
+        int intPart = integerSqrt(x);
+        double ans = intPart;
+        double increment = 0.1;
+
+        for (int i = 0; i < 3; ++i) {  // Loop to calculate up to three decimal places
+            while (ans * ans <= x) {
+                ans += increment;
+            }
+            ans -= increment;
+            increment /= 10;
+        }
+
+        return ans;
+    }
+};
